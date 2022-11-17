7a527fc5d1f0f70079730cdd29f6354abf48ac9e
Jessie Wang <jessie.wang.daylil@gmail.com>
2022-11-18 00:19:45 +0800

    init

---
+++ app/Http/Controllers/controller_album.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controller_album extends Controller
+{
+    public function hello(){
+        return view('album');
+
+    }
+}
