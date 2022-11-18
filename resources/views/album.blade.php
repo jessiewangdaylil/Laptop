e661296d00308019b3929623fe8432cbd4c12f4c
Jessie Wang <jessie.wang.daylil@gmail.com>
2022-11-18 16:28:13 +0800

    嘗試做:https://ithelp.ithome.com.tw/m/articles/10228233?fbclid=IwAR3oAMaTQKShecV8CjHYoF5iGker4kESY8wNBuvzwmZ8ShxCLfKWbIaD8VE 沒成功

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -13,28 +13,28 @@
 <section class="hero-section">
   <div class="card-grid">
     <a class="card" href="#">
-      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)"></div>
+      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1668737941502-3ef01fe1bcb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80)"></div>
       <div class="card__content">
         <p class="card__category">Category</p>
         <h3 class="card__heading">Example Card Heading</h3>
       </div>
     </a>
     <a class="card" href="#">
-      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1668738160909-9ff4efc78b7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)"></div>
       <div class="card__content">
         <p class="card__category">Category</p>
         <h3 class="card__heading">Example Card Heading</h3>
       </div>
     </a>
     <a class="card" href="#">
-      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1668738160866-9b1882633889?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)"></div>
       <div class="card__content">
         <p class="card__category">Category</p>
         <h3 class="card__heading">Example Card Heading</h3>
       </div>
     </li>
     <a class="card" href="#">
-      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557004396-66e4174d7bf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1668738160861-5312a3546dd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)"></div>
       <div class="card__content">
         <p class="card__category">Category</p>
         <h3 class="card__heading">Example Card Heading</h3>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 });
 
 Route::get('n_Album','App\Http\Controllers\controller_album@hello');
+use Intervention\Image\Facades\Image;
+
+// usage inside a laravel route
+Route::get('/', function()
+{
+    $img = Image::make('https://images.pexels.com/photos/4273439/pexels-photo-4273439.jpeg')->resize(300, 200); // 這邊可以隨便用網路上的image取代
+    return $img->response('jpg');
+});
